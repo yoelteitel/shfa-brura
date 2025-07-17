12e89cf1f90cbbd6c621e61cb9a9e9d76cfc68b4
yoelteitel <YOEL6938262@GMAIL.COM>
2025-07-17 07:32:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function selectLanguage(lang) {
+  document.getElementById('language-select').classList.add('hidden');
+  document.getElementById('main-content').classList.remove('hidden');
+}
+function startLearning() {
+  alert("התחלנו ללמוד בשפה ברורה!");
+}
